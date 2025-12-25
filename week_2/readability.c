928ae3d20c4353e16000e92fba3704a5bd16137d
toryprongs <109091794+toryprongs@users.noreply.github.com>
2025-12-25 19:54:55 +0500

    Create readability.c

---
+++ week_2/readability.c
@@ -0,0 +1,56 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+
+    int words = 1;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+    }
+
+    int sentences = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+    double L = ((double) letters / words) * 100;
+    double S = ((double) sentences / words) * 100;
+
+    double index = 0.0588 * L - 0.296 * S - 15.8;
+    int grade = round(index);
+
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
